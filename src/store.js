77cb6458712d653038f45676344cbaff695f0808
ashen-develops <ashenp.designs@gmail.com>
2020-11-11 14:31:39 -0600

    error handler

--- src/store.js
+++ src/store.js
@@ -1,22 +1,23 @@
-const bookmarks = [{
-  id: 1,
-  url: 'website',
-  description: 'This is the first Bookmark'
-},
-{
-  id: 2,
-  url: 'site',
-  description: 'This is the second Bookmark'
-},
-{
-  id: 3,
-  url: 'web',
-  description: 'This is the third Bookmark'
-},
-{
-  id: 4,
-  url: 'ebsi',
-  description: 'This is the fourth Bookmark'
-}];
+const bookmarks = [
+  {
+    id: 1,
+    url: 'website',
+    description: 'This is the first Bookmark'
+  },
+  {
+    id: 2,
+    url: 'site',
+    description: 'This is the second Bookmark'
+  },
+  {
+    id: 3,
+    url: 'web',
+    description: 'This is the third Bookmark'
+  },
+  {
+    id: 4,
+    url: 'ebsi',
+    description: 'This is the fourth Bookmark'
+  }];
 
 module.exports = {bookmarks};
