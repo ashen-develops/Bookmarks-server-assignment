faa93d27a7fb6f840aaa013472ecd32e993883ef
ashen-develops <ashenp.designs@gmail.com>
2020-11-13 13:07:14 -0600

    fixed inability to post and modified how store is called

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -11,7 +11,7 @@ const { isWebUri } = require('valid-url');
 bookmarksRouter
   .route('/bookmarks')
   .get((req, res) => {
-    res.json(store.bookmarks);
+    res.json(store);
   })
   .post(bodyParser, (req, res) => {
     for (const field of ['title', 'url', 'rating']) {
@@ -34,7 +34,7 @@ bookmarksRouter
 
     const bookmark = { id: uuid(), title, url, description, rating };
 
-    store.bookmarks.push(bookmark);
+    store.push(bookmark);
 
     logger.info(`Bookmark with id ${bookmark.id} created`);
     res
@@ -44,36 +44,38 @@ bookmarksRouter
   });
 
 
-bookmarksRouter
-  .route('bookmarks/:id')
+  bookmarksRouter
+  .route('/bookmarks/:bookmark_id')
   .get((req, res) => {
-    const {id} = req.params.id;
-    const bookmark = store.bookmarks.find(b => b.id === id);
+    const { bookmark_id } = req.params;
 
-    if(!bookmark) {
-      logger.error(`Bookmark with id ${id} not found.`);
+    // eslint-disable-next-line eqeqeq
+    const bookmark = store.find(c => c.id == bookmark_id);
+
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${bookmark_id} not found.`);
       return res
         .status(404)
         .send('Bookmark Not Found');
     }
+
     res.json(bookmark);
   })
   .delete((req, res) => {
-    const {id} = req.params;
+    const { bookmark_id } = req.params;
 
-    const bookmarkIndex = store.bookmarks.findIndex(b => b.id === id);
+    const bookmarkIndex = store.findIndex(b => b.id === bookmark_id);
 
-    if(bookmarkIndex === -1) {
-      logger.error(`Bookmark with id ${id} not found`);
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${bookmark_id} not found.`);
       return res
         .status(404)
-        .send('Not found');
+        .send('Bookmark Not Found');
     }
 
     store.bookmarks.splice(bookmarkIndex, 1);
 
-    logger.info(`Card with id ${id} deleted.`);
-
+    logger.info(`Bookmark with id ${bookmark_id} deleted.`);
     res
       .status(204)
       .end();
